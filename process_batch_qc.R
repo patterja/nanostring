29f365ad7bff1d06317b1a75e9200e510c8cd05b
Janice Patterson <patterja@ohsu.edu>
2020-04-13 21:48:30 -0700

    update pipeline and seperate qc scripts
    process_batch_qc.R was all part one script ruv_mbc.R before.
    Seperated out because the normalization process is different than the batch
    pass/fail process. Uses different matrices now see batchcorrection.R.
    
    run_processing_scripts.R pipeline scripts updated with new and updated
    validatin matrices input
    
     - normalization requires rawdata
     - batch qc requires igg subtracted

--- process_batch_qc.R
+++ process_batch_qc.R
@@ -9,97 +9,114 @@ suppressPackageStartupMessages(library(argparse))
 suppressPackageStartupMessages(library(ggplot2))
 suppressPackageStartupMessages(library(reshape2))
 suppressPackageStartupMessages(library(gridExtra))
+suppressPackageStartupMessages(library(xlsx))
 
 
 ## ARGS~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-version="1.0"
-
+version="2.0"
+matrix_version = "20200320"
 
 parser <- ArgumentParser()
-parser$add_argument("-i", help="3_IGG_SUBTRACTED.tsv", dest="input_file")
+parser$add_argument("-i", help="2_GEOMEAN_NORMALIZED.tsv", dest="input_file")
 parser$add_argument("--validation_file", type="character", 
-                    default="/Volumes/Histopathology\ Shared\ Resource/CLINICAL/Nanostring/REFERENCE_FILES/validation_samples_iggsub_normalized.txt",
+                    default=paste0("/Volumes/OHSU/CLINICAL/Nanostring/REFERENCE_FILES/validation_samples_geosamp_normalized_", matrix_version,".txt"),
                     dest="validation_file", help="validation file for controls comparison")
-parser$add_argument("--mbc_md_file", type="character", default= "/Volumes/Histopathology\ Shared\ Resource/CLINICAL/Nanostring/REFERENCE_FILES/validation_mbc_metadata.txt",
-                    dest="mbc_md_file", help="metastatic breast cancer metadata file")
-parser$add_argument("--ab_ref_file", type="character", default= "/Volumes/Histopathology\ Shared\ Resource/CLINICAL/Nanostring/REFERENCE_FILES/ANTIBODY_REFERENCE.csv",
+parser$add_argument("--pos_file", type="character", 
+                    default=paste0("/Volumes/OHSU/CLINICAL/Nanostring/REFERENCE_FILES/knownpositives.txt"),
+                    dest="pos_file", help="validation file known positive cellline and antibodies")
+parser$add_argument("--md_file", type="character", default= "/Users/patterja/Box Sync/NANOSTRING/nanostring_metadata.xlsx",
+                    dest="md_file", help="metadata file")
+parser$add_argument("--ab_ref_file", type="character", default= "/Volumes/OHSU/CLINICAL/Nanostring/REFERENCE_FILES/ANTIBODY_REFERENCE.csv",
                     dest="ab_ref_file", help="ANTIBODY_REFERENCE.csv")
 parser$add_argument("--include_bad_Ab", action="store_true", default=FALSE,
                     dest="include_bad_Ab", help="include all antibodies")
 parser$add_argument("--version", action="version", version=paste0('%(prog)s = ', version))
 
 args <- parser$parse_args()
-includeBCCL = args$includeBCCL
 
 input_file = args$input_file
 validation_file = args$validation_file
-mbc_md_file = args$mbc_md_file
+pos_file = args$pos_file
+md_file = args$md_file
 ab_ref_file = args$ab_ref_file
 include_bad_Ab = args$include_bad_Ab
 
 
 ## TEST ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-if (!grep("IGG_SUBTRACTED", input_file)){
+if (!grep("GEO", input_file)){
   print("Error: The input file is not #3: IGG_SUBTRACTED.tsv")
   stop()
-} else if (!grep("iggsub", basename(validation_file))){
+} else if (!grep("geosamp", basename(validation_file))){
   print("Error: The validation file is not #3: IGG_SUBTRACTED.tsv")
   stop()
 }
 
-    
+## GLOBAL ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
 
 #Things to include and exclue
 controls=c("MCF7","HCC1954","BT474","HeyA8","MDA468 control","MDA468+EGF")
 ctrlregex = gsub("\\+", "\\\\+", paste0(paste0(controls, collapse = "|"),"|", "MDA468"))
+
 ## FUNCTIONS ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 ## INPUT DATA ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
+pos = read.csv(pos_file, sep= "\t", row.names = 3, check.names = F)
+
+#metadata
+md = read.xlsx(file=md_file, sheetName = "nansostring_metadata", check.names=F)
+md$combined_name = paste0(md$Batch, "__", md$`Sample Name`)
+
+
 if (include_bad_Ab){
   print("keeping all antibodies: IgG and antibodies that did not perform well or did not have dynamic range")
-  #validation file
+  # validation file
   validation = read.csv(validation_file, sep= "\t", row.names = 1, check.names = F)
-  colnames(validation)=make.names(gsub("\\.1$", "", colnames(validation)))
   validation=log2(validation+1)
-  #AB FILE
+
+  # AB FILE
   ab_ref = read.csv(ab_ref_file, sep=",", stringsAsFactors=F)
-  #NEW BATCH
+  
+  # NEW BATCH
   new_batch = data.matrix(read.table(file = input_file, sep="\t", row.names=1, stringsAsFactors=F, header=T))
-  #MBC 
-  mbc_md= read.table(file = mbc_md_file, sep="\t", row.names=1, stringsAsFactors=F, header=T)
+  lnew_batch = log2(new_batch+1)
+  
+  #AB_ORDER
+  ab_order = ab_ref$X.AbID[order(ab_ref$Target)]
   
 } else {
-  omit = c("Histone H3", "S6", "RbAb-IgG", "MmAb-IgG1", "p-TSC2", "TSC2")
+  omit = c("p-TSC2", "TSC2", "NEG")
   omitregex = paste0(paste0("^", omit), collapse = "|")
+  
   #validation file
-  validation = read.csv(validation_file, sep= "\t", row.names = 1, check.names = F)
+  validation = read.csv(validation_file, sep= "\t", row.names = 1, check.names = T)
   validation = validation[!grepl(omitregex, rownames(validation)),]
-  
   lvalidation=log2(validation+1)
-  colnames(lvalidation)=make.names(gsub("\\.1$", "", colnames(lvalidation)))
-  
+ 
   #AB FILE
   ab_ref = read.csv(ab_ref_file, sep=",", stringsAsFactors=F)
   ab_ref = ab_ref[!grepl(omitregex, ab_ref$X.AbID),]
+  
   #NEW BATCH
   new_batch = data.matrix(read.table(file = input_file, sep="\t", row.names=1, stringsAsFactors=F, header=T))
   new_batch = new_batch[!grepl(omitregex, rownames(new_batch)),]
-  #MBC 
-  mbc_md= read.table(file = mbc_md_file, sep="\t", row.names=1, stringsAsFactors=F, header=T)
+  lnew_batch = log2(new_batch+1)
+
+  #AB_ORDER
+  ab_order = ab_ref$X.AbID[order(ab_ref$Target)]
+  ab_order = ab_order[!grepl(omitregex, ab_order)]
 }
 
-#AB_ORDER
-ab_order = ab_ref$X.AbID[order(ab_ref$Target)]
-ab_order = ab_order[!grepl(omitregex, ab_order)]
 
 #VALIDATION QC MATRIX #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-lctl_norm = as.matrix(lvalidation)[,grepl(ctrlregex, colnames(lvalidation))]
+control_samples = md$combined_name[md$Study %in% c("control")]
+lctl_norm = as.matrix(lvalidation[,colnames(lvalidation) %in% make.names(control_samples)])
 lctl_norm.m = melt(lctl_norm)
 lctl_norm.m$batch = sapply(strsplit(as.character(lctl_norm.m$Var2), "__"), `[`, 1)
 lctl_norm.m$ctlname = sapply(strsplit(as.character(lctl_norm.m$Var2), "__"), `[`, 2)
-lctl_norm.m$ctl_probe = factor(paste0(lctl_norm.m$ctlname,"_", lctl_norm.m$Var1))
+lctl_norm.m$ctl_probe = factor(paste0(lctl_norm.m$ctlname,"__", lctl_norm.m$Var1))
 
 
 validation_stats = data.frame(
@@ -114,23 +131,32 @@ validation_stats = data.frame(
   "mean.plus.2sd" = tapply(lctl_norm.m$value, lctl_norm.m$ctl_probe, mean) + 
     2*(tapply(lctl_norm.m$value, lctl_norm.m$ctl_probe, sd)))
 
-#only use controls 
-ctl_newbatch = melt(log2(new_batch[,make.names(controls)]+1))
-ctl_newbatch$ctl_probe = paste0(gsub("\\.1$", "", sapply(strsplit(as.character(ctl_newbatch$Var2), split = "_"), tail,1)),"_", ctl_newbatch$Var1)
-ctl_newbatch$cellline = factor(gsub("\\.1$", "", sapply(strsplit(as.character(ctl_newbatch$Var2), split = "_"), tail,1)))
+# NEWBATCH CONTROLS  #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-ctl_validation =cbind(validation_stats, "new_batch"=ctl_newbatch[match(rownames(validation_stats),ctl_newbatch$ctl_probe), "value"])
+#only use controls 
+ctl_newbatch = melt(lnew_batch[,make.names(controls)])
+ctl_newbatch$ctl_ab = paste0(ctl_newbatch$Var2,"__", ctl_newbatch$Var1)
+ctl_newbatch$cellline = ctl_newbatch$Var2
+
+# COMBINE INTO TABLE  #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+ctl_validation =cbind(validation_stats, "new_batch"=ctl_newbatch[match(rownames(validation_stats),ctl_newbatch$ctl_ab), "value"])
 ctl_validation$status = ifelse(ctl_validation$new_batch < ctl_validation$mean.minus.2sd, 
                                (ctl_validation$mean - ctl_validation$new_batch)/ctl_validation$stddev, 
                                ifelse(ctl_validation$new_batch >= ctl_validation$mean.plus.2sd, 
                                       (ctl_validation$new_batch - ctl_validation$mean)/ctl_validation$stddev, "PASS"))
 
+# KNOWN POSITIVES  #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+pos_validation = cbind(pos, ctl_validation[rownames(pos),])
+
 write.table(ctl_validation, file = "qc_controls.tsv", 
             sep="\t", quote = F, row.names = T, col.names = NA)
 
+write.table(pos_validation, file = "qc_controls_positive.tsv", 
+            sep="\t", quote = F, row.names = T, col.names = NA)
+
 ## QC LINEAR MODEL PLOTS ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-#~ Comparing newQC with old QC 
-#simple linear regression analysis
+#~ Comparing newQC with old QC with simple linear regression analysis
 
 lmfitqc = lm(new_batch ~ mean, data=ctl_validation)
 
@@ -150,8 +176,8 @@ ggReg = ggplot(lmfitqc$model, aes_string(x = names(lmfitqc$model)[2], y = names(
 twosd = 2*sd(ctl_validation$new_batch-ctl_validation$mean)
 #outdat =  ctl_validation[which((ctl_validation$new_batch-ctl_validation$mean) < -twosd|(ctl_validation$new_batch-ctl_validation$mean) > twosd),]
 outdat=ctl_validation
-outdat$cellline = sapply(strsplit(as.character(rownames(outdat)), "_"), `[`, 1)
-outdat$probe = sapply(strsplit(as.character(rownames(outdat)), "_"), `[`, 2)
+outdat$cellline = sapply(strsplit(as.character(rownames(outdat)), "__"), `[`, 1)
+outdat$probe = sapply(strsplit(as.character(rownames(outdat)), "__"), `[`, 2)
 
 baplot_ctl = ggplot(outdat) +
   geom_point(aes(x=mean, y=new_batch-mean, color=cellline)) +
@@ -159,7 +185,10 @@ baplot_ctl = ggplot(outdat) +
   geom_text(data=data.frame(x=0,y=c(-twosd, twosd)), aes(x, y), label = c("2SD", "-2SD"), color="red") +
   #geom_point(data = outdat, aes(x=mean,  y=outdat$new_batch-outdat$mean,color=outdat$cellline)) +
   #geom_text(data = outdat, aes(x=mean,  y=outdat$new_batch-outdat$mean,label=rownames(outdat), hjust=-0.1), size=3) +
-  labs(title = "Mean-Difference Plot of Celllines", x="Mean of validation batches", y="New Batch - Mean of validation batches")
+  labs(title = "Mean-Difference Plot of Celllines", x="Mean of validation batches", y="New Batch - Mean of validation batches") + 
+  theme(axis.text.x = element_text(size = 5), 
+        axis.text.y = element_text(size = 3),
+        strip.text = element_text(size=5))
 
 baplot_ab = ggplot(outdat) +
   geom_point(aes(x=mean, y=new_batch-mean, color=probe)) +
@@ -167,10 +196,12 @@ baplot_ab = ggplot(outdat) +
   geom_text(data=data.frame(x=0,y=c(-twosd, twosd)), aes(x, y), label = c("2SD", "-2SD"), color="red") +
   #geom_point(data = outdat, aes(x=mean,  y=outdat$new_batch-outdat$mean,color=outdat$probe)) +
   labs(title = "Mean-Difference Plot of Antibodies", x="Mean of validation batches", y="New Batch - Mean of validation batches") +
-  theme(legend.text=element_text(size=4))
+  theme(legend.text=element_text(size=4),
+        axis.text.x = element_text(size = 5), 
+        axis.text.y = element_text(size =3))
 
 
 print("saving QC plot")
 ggsave(filename ="QC_antibody_linear_plot.pdf", device = "pdf", plot=ggReg,width = 8, height = 8)
-ggsave(filename ="QC_antibody_meandiff_plot.pdf", device = "pdf", grid.arrange(baplot_ab, baplot_ctl),width = 8, height = 5)
+ggsave(filename ="QC_antibody_meandiff_plot.pdf", device = "pdf", grid.arrange(baplot_ab, baplot_ctl),width = 7, height = 8)
 
--- run_processing_scripts.sh
+++ run_processing_scripts.sh
@@ -1,19 +1,26 @@
 #!/bin/bash
 
+set -e
+
 #cd "/Users/patterja/Box\ Sync/NANOSTRING/data/${smmart}"
-NEWBATCH=`basename $1`
-DATA_DIR="/Volumes/Histopathology Shared Resource/CLINICAL/Nanostring"
+NEWBATCH=$(basename "$1")
+echo $NEWBATCH
+DATA_DIR="/Volumes/OHSU/CLINICAL/Nanostring"
 
-abfile="${DATA_DIR}""/""REFERENCE_FILES/ANTIBODY_REFERENCE.csv"
 ss_dir="${DATA_DIR}""/""automated_data/""$NEWBATCH"
-output_dir="${DATA_DIR}""/""output_subtracted_newfig/""$NEWBATCH"
-FILES=("${DATA_DIR}""/""automated_data/""$NEWBATCH"/*RCC)
-
-
+output_dir="${DATA_DIR}""/""output/""$NEWBATCH"
+#files
+FILES=("${DATA_DIR}""/""automated_data/"$NEWBATCH"/*RCC")
+abfile="${DATA_DIR}""/""REFERENCE_FILES/ANTIBODY_REFERENCE.csv"
+known_pos_file="${DATA_DIR}""/""REFERENCE_FILES/knownpositives.txt"
+validation3igg_file="${DATA_DIR}""/""REFERENCE_FILES/validation_samples_iggsub_normalized_20200320.txt"
+validation2geo_file="${DATA_DIR}""/""REFERENCE_FILES/validation_samples_geosamp_normalized_20200320.txt"
+validationraw_file="${DATA_DIR}""/""REFERENCE_FILES/validation_samples_rawdata_20200320.txt"
+md_file="/Users/patterja/Box Sync/NANOSTRING/nanostring_metadata.xlsx"
 #find samplesheet
 echo $NEWBATCH
 echo "${FILES[@]}"
-ss=`find "$ss_dir" -name "${NEWBATCH}*samplesheet.txt"`
+ss=$(find "${ss_dir}" -name "${NEWBATCH}*samplesheet.txt")
 echo $ss
 
 #if samplesheet exists process
@@ -21,13 +28,37 @@ if test -f "$ss"; then
     mkdir -p "${output_dir}" 
     echo "dir made"
     cd "${output_dir}" 
-    /Users/patterja/Workspace/nanostring/nanostring/proc_norm.py --samplesheet "$ss" --abfile "${abfile}" "${FILES[@]}"
+    echo `pwd`
+    /Users/patterja/Workspace/nanostring/nanostring/process_rcc.py --samplesheet "$ss" --abfile "${abfile}" ${FILES[@]}
 else
     echo "no samplesheet"
+    exit 1
 fi
 
-if [ -s 3_IGG_SUBTRACTED.tsv ]; then
-    /Users/patterja/Workspace/nanostring/nanostring/ruv_mbc.R -i 3_IGG_SUBTRACTED.tsv
+if [ -s rawdata.txt ]; then
+    /Users/patterja/Workspace/nanostring/nanostring/norm_nanostring.py --rawdata "rawdata.txt" --abfile "${abfile}"
+else
+    echo "something went wrong with processing rawdata.txt"
+    exit 1
+fi
+
+echo "finish processing rawdata, moving on to process_batch_qc.R"
+
+if [ -s 2_GEOMEAN_NORMALIZED.tsv ]; then
+    /Users/patterja/Workspace/nanostring/nanostring/process_batch_qc.R -i 2_GEOMEAN_NORMALIZED.tsv --pos_file "${known_pos_file}" --validation_file "${validation2geo_file}" --md_file "${md_file}" --ab_ref_file "${abfile}" 
 else
     echo "something went wrong with making 3_IGG_SUBTRACTED.tsv"
+    exit 1
 fi
+
+if [ -s 2_GEOMEAN_NORMALIZED.tsv ]; then
+    echo "running batch correction"
+#    /Users/patterja/Workspace/nanostring/nanostring/batch_correction_ruv.R -i "rawdata.txt" --validation_file "${validationraw_file}" --md_file "${md_file}" --ab_ref_file "${abfile}"
+    /Users/patterja/Workspace/nanostring/nanostring/batch_correction.R -i "rawdata.txt" --validation_file "${validationraw_file}" --md_file "${md_file}" --ab_ref_file "${abfile}"
+else
+    echo "something went wrong with making 2_GEOMEAN.tsv"
+    exit 1
+fi
+
+
+
